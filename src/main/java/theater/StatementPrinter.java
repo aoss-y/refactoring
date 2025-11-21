9c62fdd938cf90165b61642b342a025d422a1ce5
John Lin <john.lin3955@gmail.com>
2025-11-21 14:31:30 -0500

    Task 2.4 - Refactored loops

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -30,27 +30,35 @@ public class StatementPrinter {
      * @throws RuntimeException if one of the play types is not known
      */
     public String statement() {
-        int totalAmount = 0;
-        int volumeCredits = 0;
         final StringBuilder result = new StringBuilder("Statement for "
                 + invoice.getCustomer()
                 + System.lineSeparator());
 
+        result.append(String.format("Amount owed is %s%n",
+                usd(getTotalAmount(result))));
+        result.append(String.format("You earned %s credits%n", getTotalVolumeCredits()));
+        return result.toString();
+    }
+
+    private int getTotalAmount(StringBuilder result) {
+        int out = 0;
         for (Performance p : invoice.getPerformances()) {
-
-            volumeCredits += getVolumeCredits(p);
-
             // print line for this order
             result.append(String.format("  %s: %s (%s seats)%n",
                     getPlay(p).getName(),
                     usd(getAmount(p)),
                     p.getAudience()));
-            totalAmount += getAmount(p);
+            out += getAmount(p);
         }
-        result.append(String.format("Amount owed is %s%n",
-                usd(totalAmount)));
-        result.append(String.format("You earned %s credits%n", volumeCredits));
-        return result.toString();
+        return out;
+    }
+
+    private int getTotalVolumeCredits() {
+        int result = 0;
+        for (Performance p : invoice.getPerformances()) {
+            result += getVolumeCredits(p);
+        }
+        return result;
     }
 
     private static String usd(int amount) {
